e22ea600d1b46184e2c1969c556045cb452371a8
victornmeka <nme22001@byui.edu>
2023-01-07 07:33:09 +0100

    assignment 2 done!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,99 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello, Welcome to your course grading system");
+        string letter = "";
+        string sign = "";
+        Console.Write("Please enter your grading percentage: ");
+        int userGradeInput = int.Parse(Console.ReadLine());
+        if (userGradeInput >= 90)
+        {
+            letter = "A";
+            int getSign = userGradeInput % 10;
+            if (getSign <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (userGradeInput >= 80)
+        {
+            letter = "B";
+            int getSign = userGradeInput % 10;
+            if (getSign >= 7)
+            {
+                sign = "+";
+            }
+            else if (getSign <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (userGradeInput >= 70)
+        {
+            letter = "C";
+            int getSign = userGradeInput % 10;
+            if (getSign >= 7)
+            {
+                sign = "+";
+            }
+            else if (getSign <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (userGradeInput >= 60)
+        {
+            letter = "D";
+            int getSign = userGradeInput % 10;
+            if (getSign >= 7)
+            {
+                sign = "+";
+            }
+            else if (getSign <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (userGradeInput < 60)
+        {
+            letter = "F";
+            int getSign = userGradeInput % 10;
+            if (getSign <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        Console.WriteLine($"Your Grade is: {letter}{sign}");
+
+        if (userGradeInput > 70)
+        {
+            Console.WriteLine("Congratulations You passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, better luck next semester.");
+        }
+
+
     }
 }
