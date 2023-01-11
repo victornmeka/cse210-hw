1838ec3545fba52e16008f24c507bc38401cfd02
victornmeka <nme22001@byui.edu>
2023-01-12 00:09:14 +0100

    prep 3 assignment done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello Prep3 World! And Number Guessing Game!!!.");
+        int magicNumber;
+        int userGuess;
+        Random randomGenerator = new Random();
+        magicNumber = randomGenerator.Next(1, 100);
+        int guessCount = 0;
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+
+        {
+            do
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount = guessCount + 1;
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (userGuess != magicNumber);
+            Console.WriteLine($"It took you {guessCount} Guesses");
+            Console.Write("Do you want keep playing? ");
+            keepPlaying = Console.ReadLine();
+        }
+        Console.WriteLine("Thank you for playing.");
     }
 }
